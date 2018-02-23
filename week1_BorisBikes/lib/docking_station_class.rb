cccd1e1f3df53ec808a484f89cfdc44182a1f3d8
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-02-23 16:49:43 +0000

    Limited the amount of bikes the station can hold and return.

--- week1_BorisBikes/lib/docking_station_class.rb
+++ week1_BorisBikes/lib/docking_station_class.rb
@@ -1,13 +1,24 @@
 
-class DockingStation	
+class DockingStation
+  #attr_reader :bike	
+
+  def initialize
+    @docked_bikes = []
+  end
+
   def release_bike
-  	Bike.new
+      fail "There is no bike in the docking station!" unless @docked_bikes.count != 0
+      #Bike.new
+      @docked_bikes.pop
   end
 
   def dock(bike)
-  	@bike = bike
+  	fail "Docking station full" if @docked_bikes.count >= 20
+  	@docked_bikes << Bike.new
   end 
-  
-  attr_reader :bike  
-  
-end	
+
+end
+
+
+#bike = Bike.new
+#docked_bikes
--- week1_BorisBikes/spec/DokingStation_spec.rb
+++ week1_BorisBikes/spec/DokingStation_spec.rb
@@ -1,11 +1,21 @@
 require "docking_station_class"
 require "bike_class"
 describe DockingStation do
-    it { is_expected.to respond_to "release_bike" }
-	it "returns docked bikes" do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bike).to eq bike
+    #it { is_expected.to respond_to "release_bike" }
+	describe "#release_bike" do	
+	  it "releases docked bikes" do
+		#bike = Bike.new
+		# subject.dock(bike)
+		# expect(subject.bike).to eq bike
+	      expect { subject.release_bike }.to raise_error "There is no bike in the docking station!"
+	      #expect { bike.release_bike }.to eq 20.times { docking_station.dock Bike.new }
+	  end
+	end      
+	describe "#dock(bike)" do
+	  it "docks bikes" do   
+	    bike = Bike.new 
+	    20.times { subject.dock(bike) }
+	      expect { subject.dock(bike) }.to raise_error( 'Docking station full')
+	  end
 	end	
  end
-
